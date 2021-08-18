21f79ab2e88108baa21edba447c536ab374ba625
Stefan Ristic <risticza@gmail.com>
2021-08-18 22:56:25 +0200

    OOP constructor

---
+++ intro.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::string;
+
+class Employee {
+public:
+	string Name;
+	string Company;
+	int Age;
+
+	void IntroduceYourself() {
+		std::cout << "Name - " << Name << std::endl;
+		std::cout << "Company - " << Company << std::endl;
+		std::cout << "Age - " << Age << std::endl;
+	}
+
+	Employee(string name, string company, int age) {
+		Name = name;
+		Company = company;
+		Age = age;
+	};
+};
+
+int main()
+{
+	Employee employee1 = Employee("Nikola", "RilexYT", 25);
+	employee1.IntroduceYourself();
+
+	Employee employee2 = Employee("John", "Amazzon", 24);
+	employee2.IntroduceYourself();
+}
